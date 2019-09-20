ecb2a78e74e2c50d43aabc01555c1296ebd9d194
Manuel Muñoz Solera <mamuso@github.com>
2019-09-19 22:43:14 -0700

    defining context

--- .github/workflows/daily-template/index.js
+++ .github/workflows/daily-template/index.js
@@ -1,5 +1,6 @@
-const github = require("@actions/github");
 const core = require("@actions/core");
+const github = require("@actions/github");
+const context = github.context;
 
 const myToken = core.getInput("GITHUB_TOKEN");
 const octokit = new github.GitHub(myToken);
