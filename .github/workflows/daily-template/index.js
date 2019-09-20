5fd3d52f339bc3b45368e1e17cb462f16693cde1
Manuel Muñoz Solera <mamuso@github.com>
2019-09-19 23:07:11 -0700

    Cleaning the log

--- .github/workflows/daily-template/index.js
+++ .github/workflows/daily-template/index.js
@@ -3,10 +3,7 @@ const github = require("@actions/github");
 const context = github.context;
 
 const myToken = core.getInput("GITHUB_TOKEN");
-console.log(myToken);
-
 const octokit = new github.GitHub(myToken);
-console.log(octokit);
 
 const master = octokit.git
   .getRef({
