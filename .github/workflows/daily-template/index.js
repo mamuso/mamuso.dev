84fa7a2320c77bcf0ba469ca33338d450e198313
Manuel Muñoz Solera <mamuso@github.com>
2019-09-20 21:09:27 -0700

    don't fail

--- .github/workflows/daily-template/index.js
+++ .github/workflows/daily-template/index.js
@@ -31,59 +31,59 @@ fs.readFile(".github/daily-template.md", "utf8", function(err, data) {
 
 const path = `src/pages/${date}/index.md`;
 
-process()
-  .then(content => {
-    return content, blob;
-  })
-  .then((content, blob) => {
-    console.log("2. Create tree");
-    console.log(`content: ${content}`);
-    console.log(`blob: ${blob}`);
-    const res = octokit.git.createTree({
-      ...context.repo,
-      base_tree: context.payload.head_commit.tree_id,
-      tree: [
-        {
-          path,
-          mode: "100644",
-          type: "blob",
-          sha: blob,
-          content: content
-        }
-      ]
-    });
-    console.log("pre-res");
-    console.log(res);
-    return res;
-  })
-  .then(res => {
-    console.log("3. Create commit");
-    octokit.git.createCommit({
-      ...context.repo,
-      message: `${date}`,
-      tree: res.data.sha,
-      parents: [context.sha]
-    });
-  })
-  .then(res => {
-    console.log("4. Create ref");
-    octokit.git.createRef({
-      ...context.repo,
-      sha: res.data.sha,
-      ref: `refs/heads/${date}`
-    });
-  })
-  .then(res => {
-    console.log("5. Create pull request");
-    octokit.pulls.create({
-      ...context.repo,
-      head: `${context.repo.owner}:${context.ref.replace("refs/heads/", "")}`,
-      base: context.ref.replace("refs/", ""),
-      title: "test",
-      body: "tost"
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    context.ref.replace("refs/heads/", "");
-  });
+// process()
+//   .then(content => {
+//     return content, blob;
+//   })
+//   .then((content, blob) => {
+//     console.log("2. Create tree");
+//     console.log(`content: ${content}`);
+//     console.log(`blob: ${blob}`);
+//     const res = octokit.git.createTree({
+//       ...context.repo,
+//       base_tree: context.payload.head_commit.tree_id,
+//       tree: [
+//         {
+//           path,
+//           mode: "100644",
+//           type: "blob",
+//           sha: blob,
+//           content: content
+//         }
+//       ]
+//     });
+//     console.log("pre-res");
+//     console.log(res);
+//     return res;
+//   })
+//   .then(res => {
+//     console.log("3. Create commit");
+//     octokit.git.createCommit({
+//       ...context.repo,
+//       message: `${date}`,
+//       tree: res.data.sha,
+//       parents: [context.sha]
+//     });
+//   })
+//   .then(res => {
+//     console.log("4. Create ref");
+//     octokit.git.createRef({
+//       ...context.repo,
+//       sha: res.data.sha,
+//       ref: `refs/heads/${date}`
+//     });
+//   })
+//   .then(res => {
+//     console.log("5. Create pull request");
+//     octokit.pulls.create({
+//       ...context.repo,
+//       head: `${context.repo.owner}:${context.ref.replace("refs/heads/", "")}`,
+//       base: context.ref.replace("refs/", ""),
+//       title: "test",
+//       body: "tost"
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     context.ref.replace("refs/heads/", "");
+//   });
