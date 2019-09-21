d0d1e41f6bc26ca6f352db8bbb983d2b3fb8340c
Manuel Muñoz Solera <mamuso@github.com>
2019-09-20 21:02:14 -0700

    Is content right?

--- .github/workflows/daily-template/index.js
+++ .github/workflows/daily-template/index.js
@@ -1,27 +1,28 @@
+// Require
 const fs = require("fs").promises;
 const core = require("@actions/core");
 const github = require("@actions/github");
-const io = require("@actions/io");
 const nunjucks = require("nunjucks");
+
+// Setting the script up
+const githubtoken = core.getInput("GITHUB_TOKEN");
+const octokit = new github.GitHub(githubtoken);
 const context = github.context;
-
-const myToken = core.getInput("GITHUB_TOKEN");
-const octokit = new github.GitHub(myToken);
-
+const njenv = nunjucks.configure({ autoescape: false });
 const date = new Date().toISOString().split("T")[0];
-const template = core.getInput("filename") || ".github/daily-template.md";
-const env = nunjucks.configure({ autoescape: false });
-const templateVariables = {
-  date: date
-};
-const path = `src/pages/${date}/index.md`;
+const ref = "heads/master";
 
-console.log("1. Render template");
-async function process() {
-  return fs.readFile(template, "utf8", function(err, data) {
-    return env.renderString(data, templateVariables);
-  });
-}
+// 1. Compose the template
+const content = fs.readFile(".github/daily-template.md", "utf8", function(
+  err,
+  data
+) {
+  return njenv.renderString(data, { date: date });
+});
+
+console.log(content);
+
+const path = `src/pages/${date}/index.md`;
 
 process()
   .then(content => {
