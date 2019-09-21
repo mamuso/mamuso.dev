f55739f00ec58c326f142ac24a505f70798f9cc8
Manuel Muñoz Solera <mamuso@github.com>
2019-09-20 22:11:36 -0700

    quote

--- .github/workflows/daily-template/index.js
+++ .github/workflows/daily-template/index.js
@@ -1,18 +1,15 @@
-// Require
 const fs = require("fs").promises;
+const request = require("request");
 const core = require("@actions/core");
 const github = require("@actions/github");
 const nunjucks = require("nunjucks");
 
-// Setting the script up
 const githubtoken = core.getInput("GITHUB_TOKEN");
 const octokit = new github.GitHub(githubtoken);
 const context = github.context;
 const njenv = nunjucks.configure({ autoescape: false });
 const date = new Date().toISOString().split("T")[0];
-let content = "";
 
-// 1. Read the template
 fs.readFile(".github/daily-template.md", "utf8", function(err, data) {
   console.log(date);
 })
@@ -54,6 +51,18 @@ fs.readFile(".github/daily-template.md", "utf8", function(err, data) {
     return ref;
   })
   .then(ref => {
+    const quote = request("http://quotes.rest/qod.json", function(
+      error,
+      response,
+      body
+    ) {
+      if (!error && response.statusCode == 200) {
+        return JSON.parse(body);
+      }
+    });
+
+    console.log(quote);
+
     const pull = octokit.pulls.create({
       ...context.repo,
       head: `${context.repo.owner}:${date}`,
@@ -64,66 +73,8 @@ fs.readFile(".github/daily-template.md", "utf8", function(err, data) {
     return pull;
   })
   .then(pull => {
-    console.log("- pull");
-    console.log(pull);
-    console.log("end");
+    console.log("Pull request created!");
   })
   .catch(err => {
     console.log(err);
   });
-// process()
-//   .then(content => {
-//     return content, blob;
-//   })
-//   .then((content, blob) => {
-//     console.log("2. Create tree");
-//     console.log(`content: ${content}`);
-//     console.log(`blob: ${blob}`);
-//     const res = octokit.git.createTree({
-//       ...context.repo,
-//       base_tree: context.payload.head_commit.tree_id,
-//       tree: [
-//         {
-//           path,
-//           mode: "100644",
-//           type: "blob",
-//           sha: blob,
-//           content: content
-//         }
-//       ]
-//     });
-//     console.log("pre-res");
-//     console.log(res);
-//     return res;
-//   })
-//   .then(res => {
-//     console.log("3. Create commit");
-//     octokit.git.createCommit({
-//       ...context.repo,
-//       message: `${date}`,
-//       tree: res.data.sha,
-//       parents: [context.sha]
-//     });
-//   })
-//   .then(res => {
-//     console.log("4. Create ref");
-//     octokit.git.createRef({
-//       ...context.repo,
-//       sha: res.data.sha,
-//       ref: `refs/heads/${date}`
-//     });
-//   })
-//   .then(res => {
-//     console.log("5. Create pull request");
-//     octokit.pulls.create({
-//       ...context.repo,
-//       head: `${context.repo.owner}:${context.ref.replace("refs/heads/", "")}`,
-//       base: context.ref.replace("refs/", ""),
-//       title: "test",
-//       body: "tost"
-//     });
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     context.ref.replace("refs/heads/", "");
-//   });
