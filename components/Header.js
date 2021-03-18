78d3dfb6c0ae41cfd4f38e7f58212188cf7ca50a
Manuel Munoz Solera <mamuso@github.com>
2021-03-17 18:34:24 -0700

    Inline-styling for Chrome

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,6 @@ const HeaderWrap = styled.header`
   margin-bottom: 8rem;
 `;
 
-const SVGTag = styled.svg`
-  background-color: var(--bg-primary);
-`;
-
 const HeaderLink = styled.a`
   &:hover .pt {
     transform: translateY(0.6rem);
@@ -25,7 +21,7 @@ export default function Header() {
   return (
     <HeaderWrap>
       <HeaderLink href="/">
-        <SVGTag class="logo" width="44" height="60" viewBox="0 0 56 67" fill="none" xmlns="http://www.w3.org/2000/svg" alt="mamuso.dev">
+        <svg class="logo" width="44" height="60" viewBox="0 0 56 67" fill="none" xmlns="http://www.w3.org/2000/svg" alt="mamuso.dev" style={{ backgroundColor: "var(--bg-primary)" }}>
           <Path
             className="pt"
             d="M37.276 45.752C37.2974 45.7591 37.3188 45.7663 37.3403 45.7735C46.6806 48.8965 55.6719 49.22 55.6719 49.22C55.6719 49.22 57.7892 29.8228 51.2671 17.4271C44.745 5.03129 30.0982 0 30.0982 0C30.0982 0 -0.443344 9.04381 0.00488585 26.2813C0.453115 43.5189 12.0332 51.942 12.0332 51.942C12.0332 51.942 14.8572 49.22 18.7567 45.5765C18.8198 45.5175 18.8825 45.4589 18.9448 45.4006C19.2244 45.7139 19.5105 46.0176 19.8031 46.3101C25.828 52.3341 33.0165 54.8493 33.0165 54.8493L37.276 45.752Z"
@@ -48,7 +44,7 @@ export default function Header() {
               <stop offset="1" stop-color="#85C6FE" />
             </linearGradient>
           </defs>
-        </SVGTag>
+        </svg>
       </HeaderLink>
     </HeaderWrap>
   );
