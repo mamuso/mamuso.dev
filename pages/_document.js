c9c4daadd52abb0efc33f53cf8e06f1f4a13a06a
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-03-18 18:06:57 -0700

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="icon" type="image/png" href="/img/favicon.png"></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
