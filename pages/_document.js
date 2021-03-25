5537d5cf93a3e8284d34d005cce18e9a2905b971
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-03-24 23:13:19 -0700

    SSR styled components the right way

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
+          {this.props.styleTags}
           <link rel="icon" type="image/png" href="/img/favicon.png"></link>
         </Head>
         <body>
-          <script>0</script>
           <Main />
           <NextScript />
         </body>
