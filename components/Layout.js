570e778ae48d6733b3750d423dafeeca802347bc
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-03-25 20:10:16 -0700

    fathom config

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import Head from "next/head";
 import { BLOG_URL, BLOG_TITLE, BLOG_SUBTITLE } from "../lib/constants";
 import CSSVars from "../components/CSSVars";
-import Fathom from "../components/Fathom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -16,7 +15,6 @@ export default function Layout({ children }) {
     <>
       <Head>
         <link rel="alternate" type="application/rss+xml" title={`${BLOG_TITLE} – ${BLOG_SUBTITLE}`} href={`${BLOG_URL}/rss.xml`} />
-        <Fathom siteId="ACIFFHWV" />
       </Head>
       <CSSVars />
       <Header />
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import "../styles/index.scss";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../components/Theme";
 import Layout from "../components/Layout";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("ACIFFHWV", {
+      includedDomains: ["mamuso.dev"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Layout>
