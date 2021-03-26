286174bfe6dad0c419ecacdfbe4a1d1833d0061e
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-03-25 19:16:45 -0700

    Adding fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
 import "../styles/index.scss";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../components/Theme";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load("ACIFFHWV", {
+      includedDomains: ["mamuso.dev"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Layout>
