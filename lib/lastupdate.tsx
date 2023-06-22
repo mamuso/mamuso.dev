862afcdd64ab5bc5e5a8c88387c10e63d9063395
Manuel Muñoz Solera <mamuso@mamuso.net>
2023-06-22 00:38:16 -0700

    Changing .lastupdate placement

--- lib/lastupdate.tsx
+++ lib/lastupdate.tsx
@@ -2,10 +2,10 @@ import fs from 'fs-extra'
 
 let data = new Date().toLocaleDateString('en-us', { year: 'numeric', month: 'long' })
 
-fs.writeFile('app/.lastupdate', data, (err) => {
+fs.writeFile('.lastupdate', data, (err) => {
   if (err) console.log(err)
   else {
     console.log('The written has the following contents:')
-    console.log(fs.readFileSync('app/.lastupdate', 'utf8'))
+    console.log(fs.readFileSync('.lastupdate', 'utf8'))
   }
 })
