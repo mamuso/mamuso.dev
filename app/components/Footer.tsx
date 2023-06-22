262a1607d9fccaad77f1ee0b59b1e129237bc242
Manuel Muñoz Solera <mamuso@mamuso.net>
2023-06-22 00:46:11 -0700

    removing last update for now

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <section>
-      Last update: ⁃ <Link href="https://github.com/mamuso">GitHub</Link> ⁃ <Link href="https://www.linkedin.com/in/mamuso/">Linkedin</Link> ⁃ <Link href="mailto:mamuso@mamuso.net">Say Hi</Link>
+      mamuso.dev ⁃ <Link href="https://github.com/mamuso">GitHub</Link> ⁃ <Link href="https://www.linkedin.com/in/mamuso/">Linkedin</Link> ⁃ <Link href="mailto:mamuso@mamuso.net">Say Hi</Link>
     </section>
   )
 }
