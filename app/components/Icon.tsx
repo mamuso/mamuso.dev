f491db760439b18ae165b4c1d948576b1f7d69bc
Manuel Muñoz Solera <mamuso@mamuso.net>
2023-07-04 00:20:38 -0700

    Icons and logos

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -5,12 +5,12 @@ import { Images, CodeBlock, Article, NoteBlank } from '@phosphor-icons/react'
 export function SelectIcon({ category }: { category: string }) {
   switch (category) {
     case 'note':
-      return <NoteBlank color="var(--text-light)" />
+      return <NoteBlank color="var(--text-secondary)" />
     case 'code':
-      return <CodeBlock color="var(--text-light)" />
+      return <CodeBlock color="var(--text-secondary)" />
     case 'photo':
-      return <Images color="var(--text-light)" />
+      return <Images color="var(--text-secondary)" />
     default:
-      return <Article color="var(--text-light)" />
+      return <Article color="var(--text-secondary)" />
   }
 }
