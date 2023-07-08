61c6483d91a6342af53111f1cd15cb53c9faf7dd
Manuel Muñoz Solera <mamuso@mamuso.net>
2023-07-07 22:45:30 -0700

    Note and icons

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Images, CodeBlock, Article, NoteBlank } from '@phosphor-icons/react'
+import { Images, CodeBlock, Article, Note } from '@phosphor-icons/react'
 
 export { TreeStructure, Robot, Browsers, PencilLine, Package, SketchLogo } from '@phosphor-icons/react'
 
 export function SelectIcon({ category }: { category: string }) {
   switch (category) {
     case 'note':
-      return <NoteBlank color="var(--text-secondary)" />
+      return <Note color="var(--text-secondary)" />
     case 'code':
       return <CodeBlock color="var(--text-secondary)" />
     case 'photo':
